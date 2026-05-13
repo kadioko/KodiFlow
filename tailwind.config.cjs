202e3bec87b76c06d37ea20d58c43ef923b0210b
kadioko <87696613+kadioko@users.noreply.github.com>
2026-05-13 23:51:38 +0300

    Update dependencies to latest versions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -63,4 +62,5 @@ const config: Config = {
   },
   plugins: [],
 }
-export default config
+
+module.exports = config
